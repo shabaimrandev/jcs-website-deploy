c9bd3599f6157140558fc63b4c87616adee1e6a6
Shaba Imran <shabaimrandev@gmail.com>
2025-11-26 11:47:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener("load",()=>{const preloader=document.getElementById("preloader");const content=document.getElementById("content");preloader.classList.add("fade-out");setTimeout(()=>{preloader.style.display="none";content.style.display="block"},700)});function toggleNavBar(){let navLinks=document.getElementById("myNav");if(navLinks.className==="navigation-bar"){navLinks.className+=" responsive"}else{navLinks.className="navigation-bar"}}
+let slideIndex=0;showSlides();function showSlides(){let i;let slides=document.getElementsByClassName("mySlides");for(i=0;i<slides.length;i++){slides[i].style.display="none"}
+slideIndex++;if(slideIndex>slides.length){slideIndex=1}
+slides[slideIndex-1].style.display="block";setTimeout(showSlides,5000)}
